6a9ca3877078fa1be97ae1452e30b8e24c5cef10
conghoan1211 <hoanpham12112003@gmail.com>
2025-03-05 23:05:22 +0700

    update db connection

--- API/API/Models/Exe201Context.cs
+++ API/API/Models/Exe201Context.cs
@@ -47,7 +47,7 @@ public partial class Exe201Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("server =103.179.185.40; database = exe201;uid=sa;pwd=hoancute1@;TrustServerCertificate=true");
+        => optionsBuilder.UseSqlServer("server =103.179.185.40; database = exe201;uid=sa;pwd=Hoancute1@;TrustServerCertificate=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
